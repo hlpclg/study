220541a51cd91be2f7a49bdfa3e6a5d87bc7e441
Peter <1067553459@qq.com>
2017-08-11 00:49:22 +0800

    learning laravel elixir

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,3 @@
-var elixir = require('laravel-elixir');
-
 /*
  |--------------------------------------------------------------------------
  | Elixir Asset Management
@@ -11,6 +9,43 @@ var elixir = require('laravel-elixir');
  |
  */
 
+// #production —— 检查当前环境是否是生成环境，默认为true
+// #assetsDir —— 前端资源目录路径，默认是resources/assets
+// #cssOutput —— 所有css文件默认输出目录，默认是public/css
+// #jsOutput —— 所有JS文件输出目录，默认是public/js
+// #sourcemaps —— 是否要生成Source Map（关于什么是Source Map，可参考阮一峰的这篇文章：JavaScript Source Map 详解），默认为true
+
+
+var elixir = require('laravel-elixir');
+
+/**
+ * laravel elixir config
+ * @type {Boolean}
+ */
+elixir.config.production = false;
+// elixir.config.assetsDir = false;
+// elixir.config.cssOutput = false;
+// elixir.config.jsOutput = false;
+elixir.config.sourcemaps = false;
+
+// 
 elixir(function(mix) {
-    mix.sass('app.scss');
+    mix.sass(['app.scss', 'content.scss']);
 });
+
+// 默认生成app.css 
+// elixir(function(mix) {
+//     mix.less(['app.less', 'controllers.less']);
+// });
+
+// 指定生成目录以及文件
+// elixir(function(mix) {
+//     mix.less(['app.less', 'controllers.less'], 'public/stylesheets/style.css');
+// });
+
+// 默认生成all.css 
+// elixir(function(mix) {
+//     mix.styles([
+//         'main.css'
+//     ]);
+// });
---
+++ tasks/elixir-extensions.js
@@ -0,0 +1,30 @@
+/*
+* @Author: GuoSai
+* @Date:   2017-08-10 23:56:37
+* @Last Modified by:   Peter
+* @Last Modified time: 2017-08-10 23:59:53
+*/
+
+'use strict';
+
+
+'use strict';
+
+// File: elixir-extensions.js
+
+var gulp = require('gulp');
+var shell = require('gulp-shell');
+var Elixir = require('laravel-elixir');
+
+var Task = Elixir.Task;
+
+Elixir.extend('speak', function(message) {
+
+    new Task('speak', function() {
+        return gulp.src('').pipe(shell('say ' + message));
+    });
+
+}).watch('./app/**');
+
+
+// mix.speak('Hello World');
